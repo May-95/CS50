656f15d2f15d52bff40a3ae93827864d8339c961
May-95 <107862616+May-95@users.noreply.github.com>
2023-08-27 23:44:11 +0100

    Lab 4 — smiley

---
+++ week 4/Lab 4/smiley/helpers.c
@@ -0,0 +1,16 @@
+#include "helpers.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtRed == 00 && image[i][j].rgbtGreen == 00 && image[i][j].rgbtBlue == 00)
+            {
+                image[i][j].rgbtRed = 98;
+                image[i][j].rgbtBlue = 98;
+            }
+        }
+    }
+}
